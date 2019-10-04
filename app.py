a6fb92661f0048e78b124c640e389191867d8a23
honnguyen <honnguyen@ssis.edu.vn>
2019-10-04 17:40:22 +0700

    submit form in python

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
-
+import json
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# TODO TODO TODO GOODLUCK
+@app.route('/login_request', methods=['POST'])
+def login_request():
+    username = request.form["username"]
+    password = request.form["password"]
+    return json.dumps({'status':'OK','user':username,'pass':password});
 
 if __name__ == "__main__":
     app.run(debug=True)
